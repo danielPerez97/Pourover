9e6b1450dfe5c47d51e1da566a8534317340b5a0
Daniel Perez <doperez1997@gmail.com>
2024-11-22 22:58:25 -0700

    Updated createViewController for the ios simulator to use the createPresenter function from the :Presenter module

--- Frontend/src/iosSimulatorArm64Main/kotlin/dev/danperez/foursix/frontend/createViewController.kt
+++ Frontend/src/iosSimulatorArm64Main/kotlin/dev/danperez/foursix/frontend/createViewController.kt
@@ -6,9 +6,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.ComposeUIViewController
 import dev.danperez.foursix.presenter.FourSixPresenter
 import dev.danperez.foursix.presenter.FourSixState
+import dev.danperez.foursix.presenter.createPresenter
 import platform.UIKit.UIViewController
 
-fun createViewController(state: FourSixState, presenter: FourSixPresenter): UIViewController = ComposeUIViewController {
+fun createViewController(state: FourSixState): UIViewController = ComposeUIViewController {
+    val presenter = createPresenter()
     Box(Modifier.fillMaxSize()) {
         Screen(
             model = state,
