65c767fcb02a03906e9172d0626faa90b331ee3b
Daniel Perez <doperez1997@gmail.com>
2025-03-25 23:03:53 -0600

    Removed Activity Action bar
    Switched Column to a Scaffold and injected the paddingValues for the content

--- Frontend/src/commonMain/kotlin/dev/danperez/foursix/frontend/Frontend.kt
+++ Frontend/src/commonMain/kotlin/dev/danperez/foursix/frontend/Frontend.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package dev.danperez.foursix.frontend
 
 import PourChronology
@@ -13,10 +15,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -36,10 +41,19 @@ fun Screen(
     modifier: Modifier = Modifier,
 )
 {
-    Surface(modifier) {
+    Scaffold(
+        modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text("Pourover") }
+            )
+        }
+    ) { paddingValues ->
         Column(
             verticalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier.fillMaxSize(),
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
         ) {
             PoursData(
                 firstHalfPours = model.firstHalfPours,
